c5463cd4933f4c2842722ddf4c3e4b45f4a02bc4
didin jamaludin <didinj@gmail.com>
2018-10-09 23:17:56 +0700

    update readme

--- Firebase.js
+++ Firebase.js
@@ -4,12 +4,12 @@ import firestore from 'firebase/firestore'
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-  apiKey: "AIzaSyDtTHYPfdYCLGPp3eOJaamVvKN5IeqOqDY",
-  authDomain: "djamware-df153.firebaseapp.com",
-  databaseURL: "https://djamware-df153.firebaseio.com",
-  projectId: "djamware-df153",
-  storageBucket: "djamware-df153.appspot.com",
-  messagingSenderId: "1038790483701"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DB_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_FCM_ID"
 };
 firebase.initializeApp(config);
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # React Native Firebase Tutorial: Build CRUD Firestore App
 
-This source code is part of [React Native Firebase Tutorial: Build CRUD Firestore App]() tutorial.
+This source code is part of [React Native Firebase Tutorial: Build CRUD Firestore App](https://www.djamware.com/post/5bbcd38080aca7466989441b/react-native-firebase-tutorial-build-crud-firestore-app) tutorial.
